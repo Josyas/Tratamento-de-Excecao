cec24dd2978d8cfec3db0d9b58155f11466cc096
Josias <87546094+Josyas@users.noreply.github.com>
2022-01-17 06:36:35 -0300

    arquivos

---
+++ Exceção Personalizada/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Exceção_Personalizada.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
